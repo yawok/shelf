c7b2418f22b073184d7d30bc9db1822c8e7660e9
yaw.o.k <obengky72@gmail.com>
2023-03-11 15:36:55 +0000

    Add home url and test page

--- shelf/settings.py
+++ shelf/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "main.apps.MainConfig",
+    
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
