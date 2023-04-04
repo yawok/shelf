3fa616bd4fb1241cf1556563572e7b947d3964af
yaw.o.k <obengky72@gmail.com>
2023-04-04 23:48:05 +0000

    Test create order function

--- main/tests/test_models.py
+++ main/tests/test_models.py
@@ -3,7 +3,7 @@ from main import models
 from decimal import Decimal
 
 
-class TestModels(TestCase):
+class TestProduct(TestCase):
     def test_active_manager_works(self):
         models.Product.objects.create(
             name='A random product 1',
@@ -20,3 +20,48 @@ class TestModels(TestCase):
         )
         
         self.assertEqual(len(models.Product.objects.active()), 2)
+        
+
+class TestBasket(TestCase):
+    def test_create_order_works(self):
+        p1 = models.Product.objects.create(name="A tale of 2 cities", price=Decimal("33.5"))
+        p2 = models.Product.objects.create(name="Cell", price=Decimal("24.00"))
+        user = models.User.objects.create_user(email="user@example.com", password="1randomDud6")
+        billing = models.Address.objects.create(
+            user=user,
+            name="Random Dude",
+            address1="Dakwadwom",
+            address2="RishRishe",
+            zip_code="00012",
+            city="Kumasi",
+            country="gh"
+        )
+        shipping = models.Address.objects.create(
+            user=user,
+            name="Random Dude's brother",
+            address1="Dakwadwom",
+            address2="RishRishe",
+            zip_code="00012",
+            city="Kumasi",
+            country="gh"
+        )
+        basket = models.Basket.objects.create(user=user)
+        models.BasketLine.objects.create(basket=basket, product=p1)
+        models.BasketLine.objects.create(basket=basket, product=p2, quantity=2)
+        
+        with self.assertLogs("main.models", level="INFO") as cm:
+            order = basket.create_order(shipping, billing)
+
+        self.assertGreaterEqual(len(cm.output), 1)
+        
+        order.refresh_from_db()
+        
+        self.assertEqual(order.user, user)
+        self.assertEqual(order.shipping_name, "Random Dude's brother")
+        self.assertEqual(order.billing_name, "Random Dude")
+        self.assertEqual(order.lines.all().count(), 3)
+        lines = order.lines.all()
+        self.assertEqual(lines[0], p1)
+        self.assertEqual(lines[1], p2)
+        self.assertEqual(lines[2], p2)
+        
