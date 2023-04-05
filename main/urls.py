b8c2085d0feb930da23f41f020c511a4bd2bcf02
yaw.o.k <obengky72@gmail.com>
2023-04-05 23:03:50 +0000

    AdAdd order_done page and urls

--- main/urls.py
+++ main/urls.py
@@ -32,5 +32,6 @@ urlpatterns = [
     path("address/<int:pk>/delete/", views.AddressDeleteView.as_view(), name="address_delete"),
     path("add_to_basket/", views.add_to_basket, name="add_to_basket"),
     path("basket/", views.manage_basket, name="basket"),
-    path("address_select/", views.AddressSelectionView.as_view(), name="address_select"),
+    path("order/address_select/", views.AddressSelectionView.as_view(), name="address_select"),
+    path("order/done/", TemplateView.as_view(template_name="order_done.html"), name="checkout_done"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
