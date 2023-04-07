ee47ee195f0df422bb11b0534a3a98dcdbea4b53
yaw.o.k <obengky72@gmail.com>
2023-04-07 23:31:44 +0000

    Format with black

--- main/forms.py
+++ main/forms.py
@@ -85,9 +85,9 @@ BasketLineFormset = inlineformset_factory(
 class AddressSelectionForm(forms.Form):
     billing_address = forms.ModelChoiceField(queryset=None)
     shipping_address = forms.ModelChoiceField(queryset=None)
-    
+
     def __init__(self, user, *args, **kwargs):
         super().__init__(*args, **kwargs)
         queryset = models.Address.objects.filter(user=user)
-        self.fields['shipping_address'].queryset = queryset
-        self.fields['billing_address'].queryset = queryset
+        self.fields["shipping_address"].queryset = queryset
+        self.fields["billing_address"].queryset = queryset
