f22ce362aea3465173362c0e72ddd88bbd5ae3bf
yaw.o.k <obengky72@gmail.com>
2023-04-08 22:17:58 +0000

    Format with black

--- main/widgets.py
+++ main/widgets.py
@@ -1,8 +1,9 @@
 from django.forms.widgets import Widget
 
+
 class PlusMinusNumberInput(Widget):
-    template_name = 'widgets/plusminusnumber.html'
-    
+    template_name = "widgets/plusminusnumber.html"
+
     class Media:
         css = {"all": "css/plusminusnumber.css"}
         js = ("js/plusminusnumber.js",)
