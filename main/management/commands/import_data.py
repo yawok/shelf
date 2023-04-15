50945f05ad32048e7a76f5dccca49ae7ab05b078
yaw.o.k <obengky72@gmail.com>
2023-04-15 23:48:30 +0000

    Format with black

--- main/management/commands/import_data.py
+++ main/management/commands/import_data.py
@@ -10,52 +10,46 @@ from main import models
 
 class Command(BaseCommand):
     help = "Import products in Shelf"
-    
+
     def add_arguments(self, parser):
-        parser.add_argument('csvfile', type=open)
-        parser.add_argument('image_basedir', type=str)
+        parser.add_argument("csvfile", type=open)
+        parser.add_argument("image_basedir", type=str)
 
     def handle(self, *args, **options):
         self.stdout.write("Importing products")
         c = Counter()
-        reader = csv.DictReader(options.pop('csvfile'))
+        reader = csv.DictReader(options.pop("csvfile"))
         for row in reader:
             product, created = models.Product.objects.get_or_create(
-                name=row['name'],
-                price=row['price']
+                name=row["name"], price=row["price"]
             )
-            product.description = row['description']
-            product.slug = slugify(row['name'])
-            for import_tag in row['tags'].split('|'):
+            product.description = row["description"]
+            product.slug = slugify(row["name"])
+            for import_tag in row["tags"].split("|"):
                 tag, tag_created = models.ProductTag.objects.get_or_create(
                     name=import_tag
                 )
                 product.tags.add(tag)
-                c['tags'] += 1
+                c["tags"] += 1
                 if tag_created:
-                    c['tags_created'] += 1
+                    c["tags_created"] += 1
             with open(
-                os.path.join(
-                    options['image_basedir'],
-                    row['image_filename']
-                ), 'rb'
+                os.path.join(options["image_basedir"], row["image_filename"]), "rb"
             ) as f:
                 image = models.ProductImage(
                     product=product,
-                    image=ImageFile(f, name=row['image_filename']),
+                    image=ImageFile(f, name=row["image_filename"]),
                 )
                 image.save()
-                c['images'] += 1
+                c["images"] += 1
             product.save()
-            c['products'] += 1
+            c["products"] += 1
             if created:
-                c['products_created'] += 1
+                c["products_created"] += 1
         self.stdout.write(
             f"Products processed={c['products']} (created={c['products_created']})"
         )
         self.stdout.write(
             f"Tags processed={c['tags_created']} (created={c['tags_created']})"
         )
-        self.stdout.write(
-            f"Images processed={c['images']}"
-        )   
+        self.stdout.write(f"Images processed={c['images']}")
